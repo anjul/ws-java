f58df2eadffd527058f44bc5e55c8ea15fb9f46b
Anjul Sharma <anjulnwo@gmail.com>
2019-01-27 18:48:11 +0800

    First Commit for GLS project
    Config files

---
+++ gls/src/main/java/com/ntuc/nexus/lac/fp/gls/GlsApplication.java
@@ -0,0 +1,14 @@
+package com.ntuc.nexus.lac.fp.gls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlsApplication.class, args);
+	}
+
+}
+
---
+++ gls/src/test/java/com/ntuc/nexus/lac/fp/gls/GlsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ntuc.nexus.lac.fp.gls;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GlsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
